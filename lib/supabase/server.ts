9e2a27d47df7beb62f7170ca7289293f53111577
putraharisindo-OSIPv2 <putraharisindo@gmail.com>
2026-09-21 20:29:58 +0700

    feat: add server Supabase client

---
+++ lib/supabase/server.ts
@@ -0,0 +1,6 @@
+import { createServerClient as createClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+export async function createServerClient() {
+  const cookieStore = cookies();
+  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,{cookies:{getAll(){return cookieStore.getAll();},setAll(items){try{items.forEach(({name,value,options})=>cookieStore.set(name,value,options));}catch{}}}});
+}
